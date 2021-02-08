8eded223ca3b357100c644b15a908b60be20190e
Lowrie <mr.shenhan@hotmail.com>
2021-02-08 22:43:14 +0800

    updated deploy and added DockerFile

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,18 @@
 #!/usr/bin/env sh
 
+# 校对node版本
+node -v
+npm -v
+
+# 执行安装
+npm install
+
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
 npm run docs:build
 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
+# 拷贝部署dist
+docker --help  
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:lowrietobby/isletopia-docs.git master:gh-pages
-
-cd -
