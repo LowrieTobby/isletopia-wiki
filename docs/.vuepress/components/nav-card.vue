53d6d93ebe98d177c911973c1d3b420927322b0a
Lowrie <mr.shenhan@hotmail.com>
2021-04-10 00:31:38 +0800

    modify redme.md, add nav-card.vue, optimzed some details

---
+++ docs/.vuepress/components/nav-card.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="nav-card">
+    <a class="item"  v-for="item in items" :key="item" :href="item.link">
+      <div class="item-icon">
+        {{item.icon}}
+      </div>
+      <div class="item-text">
+        {{item.text}}
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        'items':[
+          {'icon':'📝','text':'简单介绍','link':'/guide/introduction.html'},
+          {'icon':'🤔','text':'常见问题','link':'guide/faq.html'},
+          {'icon':'🍦','text':'新手教学','link':'guide/tutorial.html'},
+          {'icon':'💎','text':'材料获取','link':'guide/material.html'},
+          {'icon':'💊','text':'魔改内容','link':'guide/modification.html'},
+
+        ]
+      }
+    }
+  };
+</script>
+
+<style scoped>
+.nav-card {
+  overflow: auto;
+  display: flex;
+  margin-bottom: 20px;
+}
+.item {
+  text-align: center;
+  min-width: 120px;
+  color: #2c3e50;
+  border-style: solid;
+  border-color: #2c3e50;
+  border-width: 3px;
+  border-radius: 9px;
+  padding: 50px 10px;
+  margin: 0px 10px;
+  transition: all 0.4s ease-in-out;
+}
+
+.item:hover {
+  transition: all 0.1s ease-in-out;
+  background-color: #42b983;
+  border-color: #85e0b7;
+  color: #fff;
+}
+
+.item-icon {
+  font-size: 1.6rem;
+}
+.item-text {
+  margin-top: 12px;
+  font-size: 1.2rem;
+  font-weight: 600;
+}
+</style>
--- docs/README.md
+++ docs/README.md
@@ -9,9 +9,23 @@ footer: Copyright © 2021 Isletopia - Powered by Vuepress
 我们的 wiki 还在完善当中，当前显示的内容不一定是最终内容
 :::
 
-## 导航
+<!-- ## 导航
 - [简单介绍](guide/introduction.html)
 - [常见问题](guide/faq.html)
 - [新手教学](guide/tutorial.html)
 - [材料获取](guide/material.html)
-- [魔改内容](guide/modification.html)
+- [魔改内容](guide/modification.html) -->
+
+## 导航
+
+  <nav-card></nav-card>
+
+## :heart: 请支持我们
+
+梦幻之屿目前仅接受无偿捐助， 欢迎一切对梦幻之屿服务器表示认可的捐助。梦幻之屿的发展与升级离不开你们的支持， 你们的微小支持将是我们前进的强大动力！
+
+梦幻之屿官方唯一捐助平台：爱发电
+
+请服主喝一杯奶茶吧：[https://afdian.net/@molean](https://afdian.net/@molean)
+
+非常感谢亲爱的玩家们的支持！
