a334169378a098742ef0d4032d548a87a8ffa916
Lowrie <mr.shenhan@hotmail.com>
2021-09-18 12:58:22 +0800

    fixed some bugs

--- docs/.vuepress/configs/themeConfig.js
+++ docs/.vuepress/configs/themeConfig.js
@@ -72,7 +72,7 @@ module.exports = {
             collapsable: false,
             sidebarDepth: 1,
             children: [
-              '冰球',
+              'bingqiu',
               'skywar',
             ]
           },
